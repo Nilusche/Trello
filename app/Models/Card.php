61019fac20ef4453a92e92b4b78f2a741ff3afb7
Nilusche Liyanaarachchi <Niluschel@outlook.de>
2022-07-06 11:33:14 +0200

    init

---
+++ app/Models/Card.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\CardList;
+use App\Models\User;
+class Card extends Model
+{
+    use HasFactory;
+
+
+    public function list(){
+        return $this->belongsTo(CardList::class, 'list_id');
+    }
+
+    public function owner(){
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
---
+++ app/Models/CardList.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Card;
+use App\Models\Board;
+class CardList extends Model
+{
+    use HasFactory;
+
+
+    public function cards(){
+        return $this->hasMany(Card::class, 'list_id');
+    }
+
+    public function board(){
+        return $this->belongsTo(Board::class, 'board_id');
+    }
+}
